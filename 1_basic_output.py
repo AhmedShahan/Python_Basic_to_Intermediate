3a35a654a779168e05bdc513a78a3da45a3a9eeb
shahan <shahan.ahmed@northsouth.edu>
2024-01-06 21:36:51 +0600

    python Basic output

---
+++ 1_basic_output.py
@@ -0,0 +1,7 @@
+# print(12345) # Printing integer number
+# print(12.345) # Printing fractional number
+# print(12.345) # Printing fractional number
+# print("A") # Printing Charecter
+# print("Shahan Ahmed") #Printing String
+# print("Hello",sep=' ')
+# print("World")
